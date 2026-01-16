4cf29c514246083fa21b2ad83c7fe0822b9248bd
Matheus <matheusfelipetrinca@gmail.com>
2026-01-15 22:27:58 -0300

    feat: adding-landing

---
+++ app/(logged-out)/layout.tsx
@@ -0,0 +1,7 @@
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function LoggedOutLayout({ children }: Props) {
+  return <div className="flex flex-col gap-4 min-h-screen items-center justify-center">{children}</div>;
+}
---
+++ app/(logged-out)/page.tsx
@@ -0,0 +1,23 @@
+import { Button } from '@/components/ui/button';
+import { PersonStandingIcon } from 'lucide-react';
+import Link from 'next/link';
+
+export default function LandingPage() {
+  return (
+    <>
+      <h1 className="flex items-center gap-2">
+        <PersonStandingIcon size={50} className="text-pink-500" /> Support Me
+      </h1>
+      <p>The best dashboard to manage custom support.</p>
+      <div className="flex items-center gap-2">
+        <Button asChild>
+          <Link href="/sign-in">Log In</Link>
+        </Button>
+        <small>or</small>
+        <Button asChild variant="outline">
+          <Link href="/sign-up">Sign Up</Link>
+        </Button>
+      </div>
+    </>
+  );
+}
